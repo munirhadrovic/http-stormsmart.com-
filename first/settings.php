16d9a9a58c4b5cc944c3bc67002efc048e4c7a31
unknown <eldinsalkoviic@gmail.com>
2015-07-09 14:31:34 +0200

    First commit

---
+++ first/settings.php
@@ -0,0 +1,25 @@
+<?php
+	$adresa[0]="11040 Plantation Rd.Fort Myers,FL 33966"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[0]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[0]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[0]="MAIN SHOWROOM";
+	$telephone[0]=8889627283;// Broj koji se poziva na dugme CALL US
+
+	$adresa[1]="2400 Vanderbilt Beach Rd. Unit #102 Naples,FL 34109"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[1]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[1]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[1]="NAPLES SHOWROOM";
+	$telephone[1]=2394039092;// Broj koji se poziva na dugme CALL US
+
+	$adresa[2]="1711 Worthington Rd. Suite 106 West Palm Beach,FL 33409"; //jako je vazno navesti dobro adresu radi boljeg poziciniranja
+	$grad[2]="";//jako je vazno ako adresa ima dva imena povezati ih sa plus
+	$skracenica[2]=""; // AKo ne znate skracenicu ostavite praznu
+	$nazivlokacije[2]="WEST PALM BEACH";
+	$telephone[2]=2392807367;// Broj koji se poziva na dugme CALL US
+
+
+
+	$companynumber=3;
+	$theme="b";//Thema koja se aplicira na sve
+	$transitionefect="turn"; //kucate tranziciju koju zelite na stranama . Vrste tranzicija : "fade", "flip", "pop", "slide", "slidedown", "slideup", "turn", "slidefade", "flow"
+?>
